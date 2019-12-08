341256420c3bd27ab5c21a557348078c6a580297
ValRat <leri.ratu@gmail.com>
2019-12-08 00:37:37 -0800

    Day 2 Part 2

--- Program.fs
+++ Program.fs
@@ -22,10 +22,13 @@ let doFirstProblem =
     result
 
 // Second Problem
-let preProcess (x: int[]) =
-    Array.set x 1 12 
-    Array.set x 2 2
-    x
+let preProcess(tape: int[], noun, verb) = 
+    Array.set tape 1 noun
+    Array.set tape 2 verb
+    tape
+
+let preProcessPartOne (x: int[]) =
+    preProcess(x, 12, 2)
 
 // Test-driving Options
 // second part of tuple is wheter or not to continue processing
@@ -69,19 +72,56 @@ let rec evaluate(tape: Option<int []>, cmdPtr) =
         | _ ->
             evaluateError(tape.Value, cmdPtr + 4)
         
-
+let evaluateAndGetResult(tape, noun, verb) =
+    let program = preProcess(tape, noun, verb)
+    let result, _ = evaluate(Some program, 0)
+    if result.IsNone
+        then None
+    else
+        Some result.Value.[0]
+    
 
 let doSecondProblem = 
     let input = readInput(@"C:\Programming\adventofcode2019\AoC\inputs\2.txt") |> Seq.head 
-    let tape = input.Split ',' |> Array.map int |> preProcess
+    let tape = input.Split ',' |> Array.map int |> preProcessPartOne
     let result, _ = evaluate(Some tape, 0)
-    if result.IsNone
-        then printf "Failed to parse tape successfully"
+    if result.IsNone then
+        printf "Failed to parse tape successfully"
     else
         printf "End Result:\n%A" result.Value
 
 
+let doSecondProblemPartTwo = 
+    let input = readInput(@"C:\Programming\adventofcode2019\AoC\inputs\2.txt") |> Seq.head 
+    let tape = input.Split ',' |> Array.map int 
+
+    let compare(tape, noun, verb) = 
+        let value = evaluateAndGetResult(tape, noun, verb)
+        if value.IsNone then
+            false
+        else
+            value.Value = 19690720
+
+    // Create generator of all possible input space
+    let allNounVerbCombos = seq {
+            for noun in 0..99 do
+                for verb in 0..99 do
+                    (noun, verb)
+            }
+        
+    // Why do I have to make an array copy? I thought this is immutable? 
+    // Ans: Lists are immutable, Arrays are not
+    // Should prefer to use F#'s immutable list rather than arrays
+    // TODO: Organize the code a bit better
+    let desiredInputs = allNounVerbCombos |> Seq.tryFind (fun  (noun, verb) -> compare(Array.copy tape, noun, verb)) 
+    if desiredInputs.IsNone then
+        printf "No value found, an error probably occured"
+    else
+        printfn "(Noun: %d, Verb: %d)" <|| desiredInputs.Value
+
+            
+
 [<EntryPoint>]
 let main argv =
-    let output = doSecondProblem 
+    let output = doSecondProblemPartTwo
     0 // return an integer exit code
