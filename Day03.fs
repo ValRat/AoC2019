ee1d6640df5d22dd02f47b90bfff9ca1e5fb4e6d
ValRat <leri.ratu@gmail.com>
2019-12-10 13:01:37 -0800

    fixed directions, still something wrong

--- Day03.fs
+++ Day03.fs
@@ -3,7 +3,8 @@ module Day03
 open System.IO
 open System.Collections.Generic
 
-let input = File.ReadAllLines(@"./inputs/3test.txt")
+// let input = File.ReadAllLines(@"./inputs/3.txt")
+let input = File.ReadAllLines(@"C:/Source/AoC2019/inputs/3test.txt")
 
 let sumTuple(a: int * int, b: int * int) =
     let a1, a2 = a
@@ -23,32 +24,33 @@ let Part1 =
                     grid <- grid.Add(currCoord, grid.TryFind(currCoord).Value + 1)
                 else
                     grid <- grid.Add(currCoord, 1)
-        ()
 
         let traceSegment(seg: string) = 
             match seg.[0] with
-            | 'U' -> doTraceSegment((0, 1), seg.[1..] |> int)
-            | 'D' -> doTraceSegment((0, -1), seg.[1..] |> int)
-            | 'L' -> doTraceSegment((-1, 0), seg.[1..] |> int)
-            | 'R' -> doTraceSegment((0, 1), seg.[1..] |> int)
+            | 'U' -> doTraceSegment(( 0,  1), seg.[1..] |> int)
+            | 'D' -> doTraceSegment(( 0, -1), seg.[1..] |> int)
+            | 'L' -> doTraceSegment((-1,  0), seg.[1..] |> int)
+            | 'R' -> doTraceSegment(( 1,  0), seg.[1..] |> int)
             | _ -> ()
 
         Array.iter traceSegment segments
-    
+
     input |> Array.map (fun s -> s.Split ',' |> traceWire) |> ignore
 
-    let findShortestIntersection =
+
+    let findShortestIntersection(inputGrid) =
+        let absSumTuple(tup) = 
+            let t1, t2 = tup
+            abs(t1) + abs(t2)
 
         let getDistance(kvp: KeyValuePair<int*int,_>) = 
-            let a1, a2 = kvp.Key
-            abs(a1) + abs(a2)
+            absSumTuple(kvp.Key)
 
-        let shortestIntersection = grid |> Map.filter (fun _ v -> v > 1) 
+        let shortestIntersection = inputGrid |> Map.filter (fun _ v -> v > 1)  // Find intersections
+                                        // |> Map.filter (fun k _-> (k |> absSumTuple) = 0)  // Filter out origin if needed
                                         |> Seq.minBy getDistance
                                         |> getDistance
+        shortestIntersection
 
-        printfn "%d" shortestIntersection
-
-    findShortestIntersection |> ignore
-
-
+    let closestIntersection = findShortestIntersection(grid) 
+    printfn "%d" closestIntersection
