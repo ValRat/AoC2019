a5ee54c3dc69546aba8a0e6ed36e6faa46058bb8
ValRat <leri.ratu@gmail.com>
2019-12-10 20:21:44 -0800

    Day 3 Part 1 Done

--- Day03.fs
+++ Day03.fs
@@ -3,8 +3,7 @@ module Day03
 open System.IO
 open System.Collections.Generic
 
-// let input = File.ReadAllLines(@"./inputs/3.txt")
-let input = File.ReadAllLines(@"C:/Source/AoC2019/inputs/3test.txt")
+let input = File.ReadAllLines(@"./inputs/3.txt")
 
 let sumTuple(a: int * int, b: int * int) =
     let a1, a2 = a
@@ -12,18 +11,20 @@ let sumTuple(a: int * int, b: int * int) =
     (a1+b1, a2+b2)
 
 let Part1 = 
-    let mutable grid = Map.empty
+    let mutable grid: Map<int*int, Set<int>> = Map.empty
 
-    let traceWire (segments: string[]) =
+    let traceWire (segments) =
+        let (segs: string[], id: int) = segments
+
         let mutable currCoord = (0, 0)
 
         let doTraceSegment(dir: int * int, numSteps) =
-            for x in 1..numSteps do
+            for _ in 1..numSteps do
                 currCoord <- sumTuple(currCoord, dir)
                 if grid.ContainsKey(currCoord) then
-                    grid <- grid.Add(currCoord, grid.TryFind(currCoord).Value + 1)
+                    grid <- grid.Add(currCoord, grid.TryFind(currCoord).Value.Add(id))
                 else
-                    grid <- grid.Add(currCoord, 1)
+                    grid <- grid.Add(currCoord, Set.empty.Add(id))
 
         let traceSegment(seg: string) = 
             match seg.[0] with
@@ -31,22 +32,23 @@ let Part1 =
             | 'D' -> doTraceSegment(( 0, -1), seg.[1..] |> int)
             | 'L' -> doTraceSegment((-1,  0), seg.[1..] |> int)
             | 'R' -> doTraceSegment(( 1,  0), seg.[1..] |> int)
-            | _ -> ()
+            | _ -> failwith "UNRECOGNIZED VALUE"
 
-        Array.iter traceSegment segments
+        Array.iter traceSegment segs
 
-    input |> Array.map (fun s -> s.Split ',' |> traceWire) |> ignore
+    input |> Array.map (fun s -> s.Split ',') |> Array.mapi (fun id s -> (s, id)) |> Array.iter traceWire |> ignore
 
 
-    let findShortestIntersection(inputGrid) =
+    // Having to define the types for these means I'm probably doing something wrong...
+    let findShortestIntersection(inputGrid: Map<int*int, Set<int>>) =
         let absSumTuple(tup) = 
-            let t1, t2 = tup
+            let (t1, t2) = tup
             abs(t1) + abs(t2)
 
         let getDistance(kvp: KeyValuePair<int*int,_>) = 
             absSumTuple(kvp.Key)
 
-        let shortestIntersection = inputGrid |> Map.filter (fun _ v -> v > 1)  // Find intersections
+        let shortestIntersection = inputGrid |> Map.filter (fun _ v -> v.Count > 1)  // Find intersections
                                         // |> Map.filter (fun k _-> (k |> absSumTuple) = 0)  // Filter out origin if needed
                                         |> Seq.minBy getDistance
                                         |> getDistance
